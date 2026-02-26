fcb938a8bd32295d571a8faf10f508b59d50e398
ngkhoa20012007-bit <ngkhoa20012007@gmail.com>
2026-02-26 11:14:29 +0700

    nộp lab7

--- bai2.c
+++ bai2.c
@@ -1,6 +1,6 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
+ * Họ và tên: [Nguyễn Vũ Đăng Khoa]
+ * MSSV:      [PS49647]
  * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
  *****************************************************************************/
 
@@ -9,4 +9,24 @@
 //  Output: Đăng nhập thành công hay không thành công
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char username[50], password[50];
+    char correctUsername[] = "admin";
+    char correctPassword[] = "123456";
+
+    printf("Nhap username: ");
+    scanf("%s", username);
+    printf("Nhap password: ");
+    scanf("%s", password);
+
+    if(strcmp(username, correctUsername) == 0 && strcmp(password, correctPassword) == 0) {
+        printf("Dang nhap thanh cong!\n");
+    } else {
+        printf("Dang nhap that bai!\n");
+    }
+
+    return 0;
+}
